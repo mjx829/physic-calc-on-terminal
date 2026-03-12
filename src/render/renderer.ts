046e76081daa1177c9c6ba2b61eddb533c34da62
mjx829 <sbtk666+buin@gmail.com>
2026-03-13 07:41:54 +0900

    feat: フレーム毎の差分検出と描画処理の実装

--- src/render/renderer.ts
+++ src/render/renderer.ts
@@ -1,15 +1,34 @@
-import { Grid } from "@/types"
+import { Grid, Cell } from "@/types"
 import { initGrid } from "../physics/init"
+
+const isSameGrids = (gridA: Grid, gridB: Grid): boolean => {
+    if (gridA.length !== gridB.length) return false;
+    if (gridA[0].length !== gridB[0].length) return false;
+    
+    return true;
+}
 
 const moveCursor = (x: number, y: number): void => {
     process.stdout.write(`\e[${x};${y}H`);
 }
 
-const drawCell = (x: number, y: number, char: string): void => {
+const drawCell = (x: number, y: number, cell: Cell): void => {
     moveCursor(x, y);
-    process.stdout.write(char);
+    if (cell) {
+        process.stdout.write(cell.char);
+    } else {
+        process.stdout.write("");
+    }
 }
 
-const render = (grid: Grid): void => {
-    
+const compareGrids = (prevGrid: Grid, currGrid: Grid) => {
+    if (!isSameGrids(prevGrid, currGrid)) return false;
+
+    for (let y = 0; y < currGrid.length; y++) {
+        for (let x = 0; x < currGrid[y].length; x++) {
+            if (currGrid[y][x] !== prevGrid[y][x]) {
+                drawCell(x, y, currGrid[y][x]);
+            }
+        }
+    }
 }
