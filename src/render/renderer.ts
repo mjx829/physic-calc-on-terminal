ea21a40db84938c6ea68584b24742ebca075c0da
mjx829 <sbtk666+buin@gmail.com>
2026-03-14 19:16:43 +0900

    fix: 描画方法の変更

--- src/render/renderer.ts
+++ src/render/renderer.ts
@@ -4,6 +4,7 @@ import { initGrid, copyGrid } from "@/core/grid";
 
 let prevGrid: Grid = initGrid();
 
+/*
 const moveCursor = (x: number, y: number): void => {
     process.stdout.write(`\x1b[${y};${x * RENDER.CELL_WIDTH}H`);
 }
@@ -16,15 +17,19 @@ const drawCell = (x: number, y: number, cell: Cell): void => {
         process.stdout.write(" ");
     }
 }
+*/
 
 const renderDiff = (prevGrid: Grid, currGrid: Grid): void => {
+    let output = '';
     for (let y = 0; y < currGrid.length; y++) {
         for (let x = 0; x < currGrid[y].length; x++) {
             if (currGrid[y][x] !== prevGrid[y][x]) {
-                drawCell(x, y, currGrid[y][x]);
+                output += `\x1b[${y};${x * RENDER.CELL_WIDTH}H`;
+                output += currGrid[y][x]?.char ?? ' ';
             }
         }
     }
+    if (output) process.stdout.write(output);
 }
 
 export const render = (currGrid: Grid): void => {
